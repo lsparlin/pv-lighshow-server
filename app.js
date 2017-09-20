c12e4d2751be3bdfb6b4f35bc7598e360481c86a
Lewis Sparlin <lewis@edatasource.com>
2017-09-20 15:02:48 -0500

    Created tiny express server with socket.io

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express')
+var ioProm = require('express-socket.io')
+var cors = require('cors')
+
+const app = express()
+var port = process.env.port || 3000
+var server = ioProm.init(app)
+
+app.use(cors())
+// {credentials: true, origin: 'http://localhost:8080'}
+app.get('/', (req, res) => {
+  res.send({status: 'success'})
+})
+
+app.put('/color/:color', (req, res) => {
+  console.log(req.params)
+  ioProm.then(io => io.emit('change-color', {color: req.params.color}))
+
+  res.send()
+})
+
+ioProm.then(io => {
+  io.on('connection', (socket) => {
+    console.log('connected')
+    socket.on('test', () => console.log('test received'))
+  })
+})
+
+server.listen(port, () => {
+  console.log('listening on ' + port)
+})
