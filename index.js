2546a45e86982d94a2d12ef8a993ea2153f1f68e
Lewis Sparlin <lewis.sparlin@icloud.com>
2017-09-23 15:21:55 -0500

    Some little changes for domain/ip limiting

--- index.js
+++ index.js
@@ -7,13 +7,18 @@ var port = process.env.PORT || 3000
 var server = ioProm.init(app)
 
 app.use(cors())
+app.use(function(req, res, next) {
+  console.log('access interceptor', req.domain, req.ip)
+  next();
+  //res.status(403).end('forbidden');
+});
+
 // {credentials: true, origin: 'http://localhost:8080'}
 app.get('/', (req, res) => {
-  res.send({status: 'success'})
+  res.send({status: 'success', info: {ip: req.ip, domain: req.domain}})
 })
 
 app.put('/color/:color', (req, res) => {
-  console.log(req.params)
   ioProm.then(io => io.emit('change-color', {color: req.params.color}))
 
   res.send()
@@ -21,8 +26,6 @@ app.put('/color/:color', (req, res) => {
 
 ioProm.then(io => {
   io.on('connection', (socket) => {
-    console.log('connected')
-    socket.on('test', () => console.log('test received'))
   })
 })
 
