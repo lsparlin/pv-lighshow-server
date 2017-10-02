eaf1345214754badadf6338de2a202781af3a1b6
Lewis Sparlin <lewis@edatasource.com>
2017-10-02 14:54:50 -0500

    Removed express-socket-io dependency for pure socket.io

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const expBodyParser = require('body-parser')
-const ioProm = require('express-socket.io')
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 const Password = require('password-hash-and-salt')
@@ -9,30 +8,34 @@ const ColorDuration = require('./src/js/ColorDuration.js')
 const SequenceStore = require('./src/js/SequenceStore.js')
 
 const app = express()
+app.use(cors())
+app.use(expBodyParser.json())
 var port = process.env.PORT || 3000
-var server = ioProm.init(app)
-var db
+const server = app.listen(port, () => {
+  console.log('listening on ' + port)
+})
+const io = require('socket.io')(server)
+io.on('connection', (socket) => {
+  console.log('a client connected')
+  socket.on('disconnect', () => console.log('a client disconnected'))
+})
 
+var db
 var mongodb_url = process.env.MONGODB_URI ||  'mongodb://127.0.0.1:27017/pv_lightshow'
 const SEQ_COLLECTION_NAME = 'color_sequence'
 
 MongoClient.connect(mongodb_url, (err, database) => {
   if (err) return console.log(err)
   db = database
-  server.listen(port, () => {
-    console.log('listening on ' + port)
-  })
 })
 
-app.use(cors())
-app.use(expBodyParser.json())
 
 app.get('/', (req, res) => {
   res.send({status: 'success', info: {ip: req.ip, domain: req.domain}})
 })
 
 app.put('/color/:color', (req, res) => {
-  ioProm.then(io => io.emit('change-color', {color: req.params.color}))
+  io.emit('change-color', {color: req.params.color})
 
   res.send()
 })
@@ -43,7 +46,7 @@ app.put('/sequence/:name', (req, res) => {
     if (seq) {
       const setAndSchedule = (colorArray, index) => {
         colorObj = colorArray[index]
-        ioProm.then(io => io.emit('change-color', {color: colorObj.color}))
+        io.emit('change-color', {color: colorObj.color})
 
         if (index < colorArray.length - 1) {
           nextColor = colorArray[index+1]
@@ -109,8 +112,4 @@ app.get('/test_auth', (req, res) => {
   }
 })
 
-ioProm.then(io => {
-  io.on('connection', (socket) => {
-  })
-})
 
